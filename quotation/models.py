49d162b6a894db6d9f07d3460900d521003d9b9e
MatheusLealAquino <matheusaquino@id.uff.br>
2020-02-23 12:08:43 -0300

    creation of model of Demand and this related models

--- quotation/models.py
+++ quotation/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.core.validators import RegexValidator
+from mytools.models import OwnedModel
 
-# Create your models here.
+class State(models.Model):
+  name = models.CharField(max_length=2, blank=False, unique=True)
+
+class City(models.Model):
+  name = models.CharField(max_length=50, blank=False, unique=True)
+  state = models.ForeignKey(State, on_delete=models.CASCADE)
+
+class Demand(OwnedModel):
+  created = models.DateTimeField(auto_now_add=True)
+  description = models.TextField(blank=False)
+  street_name = models.TextField(blank=False)
+  number_address = models.DecimalField(default=0)
+  city = models.ForeignKey(City, on_delete=models.CASCADE)
+  email = models.EmailField(default='')
+  cellphone = models.CharField(
+    max_length=50,
+    required=True,
+    validators=[
+      RegexValidator(
+        regex='(\(?\d{2}\)?\s)?(\d{4,5}\-\d{4})'
+      )
+    ]
+  )
+  status = models.BooleanField(default=False)
