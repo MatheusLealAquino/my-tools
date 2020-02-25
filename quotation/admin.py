4dc3589fc570d2ff13bf2c28150e8827425c126f
MatheusLealAquino <matheusaquino@id.uff.br>
2020-02-25 11:48:39 -0300

    show advertiser name

--- quotation/admin.py
+++ quotation/admin.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 from django.utils.html import format_html
 
 from quotation.models import Demand, City, State
+from django.contrib.auth.models import User
 
 class DemandAdmin(admin.ModelAdmin):
-  list_display = ('description', 'street_name', 'number_address', 'city', 'cep', 'email', 'cellphone', 'is_ended')
+  list_display = ('description', 'street_name', 'number_address', 'city', 'cep', 'email', 'cellphone', 'advertiser', 'is_ended')
 
   def is_ended(self, obj):
     if obj.status:
@@ -12,7 +13,12 @@ class DemandAdmin(admin.ModelAdmin):
     else:
       return format_html('<img src="/static/admin/img/baseline-highlight_off.svg" alt="False">')
 
+  def advertiser(self, obj):
+    user = User.objects.get(id=obj.owner_id)
+    return user.first_name + ' ' + user.last_name
+
   is_ended.short_description = 'Status'
+  advertiser.short_description = 'Advertiser'
 
 admin.site.register(Demand, DemandAdmin)
 admin.site.register(City)
